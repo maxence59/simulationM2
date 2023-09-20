6e5908745012e7eafe0f0970e7364dc71a334fdc
maxence <roussel59330@gmail.com>
2023-09-20 11:40:36 +0200

    debut

---
+++ src/main/java/Service.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class Service {
+    private String nom;
+    private Terminal origine;
+    private Terminal destination;
+    private int capacite;
+    private List<Terminal> etapes;
+}
