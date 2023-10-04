868b0016ced0cd5f59920129789cc2a1a3f3861b
maxence <roussel59330@gmail.com>
2023-10-04 10:26:46 +0200

    l

--- src/main/java/Terminal.java
+++ src/main/java/Terminal.java
@@ -4,4 +4,12 @@ public class Terminal {
     public Terminal(String nom) {
         this.nom = nom;
     }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 }
